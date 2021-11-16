296bb046583c54ddf1f568bd7f1112038e18c761
kmem1 <konstantinovich.aleks@mail.ru>
2021-11-16 14:19:46 +0300

    Initial commit

---
+++ app/src/main/java/com/clownteam/notepad/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.clownteam.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
